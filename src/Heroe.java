634df3fce51f306b8fac92ae5517b74350362749
Gabriel Villacis <gabuandre2710@gmail.com>
2025-05-01 14:23:29 -0500

    primer commit1

---
+++ src/Heroe.java
@@ -0,0 +1,17 @@
+public class Heroe {
+    private String nombre;
+    private String raza;
+    private int Nivel;
+    private String Habilidad;
+
+    public Heroe(String nombre, String raza, int nivel, String habilidad) {
+        this.nombre = nombre;
+        this.raza = raza;
+        Nivel = nivel;
+        Habilidad = habilidad;
+    }
+
+    public String nombre(String nombre){
+
+    }
+}
