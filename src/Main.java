514e76c3e1921aacdcca2269060d1239ba708e00
Gabriel Villacis <gabuandre2710@gmail.com>
2025-05-01 15:01:09 -0500

    segundo commit

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ import java.util.*;
 
 
 public class Main {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        Queue<String>cola =new linkedlist<>();
-        String option;
+    public static void main(String[] args) {
 
     }
 }
